f98befac58bb4e1037382f1d973d04b1225d3f23
Diogo Francisco <149070174+diogopereira2006@users.noreply.github.com>
2024-09-27 17:32:52 -0300

    Add files via upload
    Um site de compras brawl stars, aonde vai conter gemas, passes, skins raras e tabela de eventos

---
+++ dswebatualizado/js/login.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+  function clicklogin(){
+    if(form.style.display == 'block'){
+      form.style.display == 'none'
+    }else{
+      form.style.display = 'block'
+    }
+  }
